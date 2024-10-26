cb078e6c38576f7f4968272a023fedd8dd2ca5b5
ubaidawan <malikunknown633@gmail.com>
2024-10-26 11:43:00 +0500

    added basic requirments

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='flex'>
+
+      <h1>Vite + React</h1>
+      <p className='text-lg bg-red-700'>some para</p>
+    </div>
+     
+      
+    </>
+  )
+}
+
+export default App
