3cbc65935a5eb308a965b4406180189461f70773
ubaidawan <malikunknown633@gmail.com>
2024-10-26 17:17:11 +0500

    change bg color on scroll - navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -15,9 +15,21 @@
 
 // export default Navbar;
 
-import React from "react";
+import React, { useEffect } from "react";
+import { useWindowScroll } from "react-use";
 
 const Navbar = () => {
+  const { y } = useWindowScroll();
+
+  useEffect(() => {
+    const header = document.querySelector("header");
+    if (header) {
+      const scrolled = y > 0;
+      header.classList.toggle("bg-black-transparent", scrolled);
+      header.classList.toggle("bg-transparent", !scrolled);
+    }
+  }, [y]);
+
   return (
     <header className="fixed top-0 w-full z-20 px-2 py-4 bg-[#09090B]border-b border-gray-800">
       <div className="text-center  flex justify-center items-center space-x-12">
